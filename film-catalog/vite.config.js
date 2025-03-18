cfe49155dfc53b5185693c661fc3000aa1846e3b
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-03-18 17:30:33 +0200

    add base url

---
+++ film-catalog/vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/new-theory_vue-todo-app_composition-api/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
